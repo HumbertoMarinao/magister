f5518c9237b22a891c80dc335aef0dd3e64af844
HumbertoMarinao <50997339+HumbertoMarinao@users.noreply.github.com>
2025-10-19 16:07:36 -0300

    Create script_prueba_lectura_archivo.py

---
+++ script_prueba_lectura_archivo.py
@@ -0,0 +1,17 @@
+import kagglehub
+import pandas as pd
+import os
+
+# Descargar la última versión del dataset
+path = kagglehub.dataset_download("redwankarimsony/heart-disease-data")
+
+print("Path to dataset files:", path)
+
+# Construir la ruta completa al archivo CSV
+csv_file_path = os.path.join(path, 'heart_disease_uci.csv')
+
+# Importar el dataset a un DataFrame de pandas
+df = pd.read_csv(csv_file_path)
+
+# Mostrar las primeras filas del DataFrame
+display(df.head())
